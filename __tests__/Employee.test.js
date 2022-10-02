c8370e540a215699c698f2ae66bfa23f3ad479f3
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-02 11:56:04 -0500

    created and tested Employee constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,34 @@
 const Employee = require('../lib/Employee.js');
+
+test('create an employee object', () => {
+  const employee = new Employee('John Miller', 'abc-12345', 'jmiller@webdev.io', 501);
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(String));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.office).toEqual(expect.any(Number));
+})
+
+test("get the employee's name", () => {
+  const employee = new Employee('John Miller', 'abc-12345', 'jmiller@webdev.io', 501);
+
+  expect(employee.getName()).toEqual(expect.any(String));
+})
+
+test("get the employee's ID", () => {
+  const employee = new Employee('John Miller', 'abc-12345', 'jmiller@webdev.io', 501);
+
+  expect(employee.getId()).toEqual(expect.any(String));
+})
+
+test("get the employee's email", () => {
+  const employee = new Employee('John Miller', 'abc-12345', 'jmiller@webdev.io', 501);
+
+  expect(employee.getEmail()).toEqual(expect.any(String));
+})
+
+test("get the employee's role", () => {
+  const employee = new Employee('John Miller', 'abc-12345', 'jmiller@webdev.io', 501);
+
+  expect(employee.getRole()).toBe('Employee');
+})
---
+++ index.js
@@ -0,0 +1,17 @@
+// inquirer.prompt (
+//   {
+//     type: 'input',
+//     name: 'managerName',
+//     message: "Eneter the team manager's name:",
+//     validate: managerInput => {
+//       if (managerInput) {
+//         return true;
+//       } else {
+//         console.log("Please enter the team manager's name:");
+//       }
+//     }
+//   }
+// )
+// .then (({name}) => {
+//   this.employee = new Employee(name);
+// })
