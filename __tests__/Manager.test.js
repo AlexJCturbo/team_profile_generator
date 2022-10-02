6b712f2a36144c4357ffab720f36f83b14504dd5
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-02 12:42:16 -0500

    created and tested Manager constructor

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,3 @@
-//const Employee = require('../lib/Employee.js');
 const Manager = require('../lib/Manager.js');
 
 test("create the manager's office number object", () => {
@@ -10,5 +9,5 @@ test("create the manager's office number object", () => {
 test('test override to return Manager', () => {
   const manager = new Manager('John Miller', 'abc-12345', 'jmiller@webdev.io', '501');
 
-  expect(manager.getRole).toBe('Manager');
+  expect(manager.getRole()).toBe('Manager');
 })
