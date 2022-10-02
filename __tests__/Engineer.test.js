408f4f4c5dd6453a2c0f4227106a150047806a2e
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-02 13:42:21 -0500

    created and tested Engineer object and methods

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,19 +1,19 @@
 const Engineer = require('../lib/Engineer.js');
 
 test("create the engineer's github username object property", () => {
-  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+  const engineer = new Engineer('Peter Evans', 'ccc-12345', 'pevans@webdev.io', 'AlexJCturbo');
 
   expect(engineer.github).toEqual(expect.any(String));
 });
 
 test("get the engineer's GitHub", () => {
-  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+  const engineer = new Engineer('Peter Evans', 'ccc-12345', 'pevans@webdev.io', 'AlexJCturbo');
 
   expect(engineer.getGithub()).toEqual(expect.any(String));
 })
 
 test('test override to return Engineer', () => {
-  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+  const engineer = new Engineer('Peter Evans', 'ccc-12345', 'pevans@webdev.io', 'AlexJCturbo');
 
   expect(engineer.getRole()).toBe('Engineer');
 });
