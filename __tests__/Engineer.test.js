360395860c6fe980cbbc9776b7a04b9b2b294363
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-02 13:40:20 -0500

    created and teste Engineer object and methods

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,19 @@
-const Employee = require('../lib/Employee.js');
 const Engineer = require('../lib/Engineer.js');
+
+test("create the engineer's github username object property", () => {
+  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+
+  expect(engineer.github).toEqual(expect.any(String));
+});
+
+test("get the engineer's GitHub", () => {
+  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+
+  expect(engineer.getGithub()).toEqual(expect.any(String));
+})
+
+test('test override to return Engineer', () => {
+  const engineer = new Engineer('Peter Evans', 'ccc-22222', 'pevans@webdev.io', 'AlexJCturbo');
+
+  expect(engineer.getRole()).toBe('Engineer');
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require('../lib/Manager.js');
 
-test("create the manager's office number object", () => {
+test("create the manager's office number object property", () => {
   const manager = new Manager('John Miller', 'abc-12345', 'jmiller@webdev.io', '501');
 
   expect(manager.office).toEqual(expect.any(String));
-})
+});
 
 test('test override to return Manager', () => {
   const manager = new Manager('John Miller', 'abc-12345', 'jmiller@webdev.io', '501');
 
   expect(manager.getRole()).toBe('Manager');
-})
+});
