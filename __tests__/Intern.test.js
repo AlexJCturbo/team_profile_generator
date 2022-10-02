4f8d7cd37cb978da31edbc6f71230e5039cecef8
AlexJCturbo <jaimes.ac.5@gmail.com>
2022-10-02 13:55:09 -0500

    created and tested Engineer and Intern objects and methods

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,19 @@
-const Employee = require('../lib/Employee.js');
 const Intern = require('../lib/Intern.js');
+
+test("create the intern's school object property", () => {
+  const intern = new Intern('Emma Brown', 'iii-123', 'intern123@webdev.io', 'UofT');
+
+  expect(intern.school).toEqual(expect.any(String));
+});
+
+test("get the intern's school name", () => {
+  const intern = new Intern('Emma Brown', 'iii-123', 'intern123@webdev.io', 'UofT');
+
+  expect(intern.getSchool()).toEqual(expect.any(String));
+})
+
+test('test override to return Engineer', () => {
+  const intern = new Intern('Emma Brown', 'iii-123', 'intern123@webdev.io', 'UofT');
+
+  expect(intern.getRole()).toBe('Intern');
+});
